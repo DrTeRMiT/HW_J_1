3626275ab5b59ba424d7f4b0fb2756fda1035029
TeRMiT <termitspy@gmail.com>
2023-02-11 19:00:27 +0300

    доделал до конца. УРА!

--- HW_J_1.java
+++ HW_J_1.java
@@ -3,7 +3,21 @@ import java.util.Scanner;
 
 public class HW_J_1 {
 
-   
+    public static void main(String[] args) {
+
+        System.out.println(triangle(number()));
+    }
+    public static int number() {
+           
+        Scanner in = new Scanner(System.in);
+        System.out.print("Input a number: ");
+        int num = in.nextInt();
+        in.close();
+        
+        return num;
+    }
+
+    
     public static int triangle(int n) {
         if(n==1)
             return 1;
